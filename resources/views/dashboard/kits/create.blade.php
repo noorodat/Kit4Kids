459c7c80184d35438f4d5d8cc8917cdad89d52fd
Your Name <you@example.com>
2023-09-13 22:17:00 +0300

    raghad3

--- resources/views/dashboard/kits/create.blade.php
+++ resources/views/dashboard/kits/create.blade.php
@@ -20,28 +20,42 @@
 <div class="card">
         <div class="card-header">Create new Kit</div>
         <div class="card-body">
-            <form action="{{ route('kits.index') }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('kits.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                     <label for="UserName">Kit Title</label>
                     <input type="text" name="title" class="form-control" 
+                     
                         placeholder="Kit Title">
+
+                        @error('title')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
                 </div>
                 <div class="form-group">
                     <label for="description">Kit Description</label>
                     <textarea name="description" class="form-control" placeholder="Kit Description"></textarea>
+                    @error('description')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 
                 <div class="form-group">
                         <label for="UserPassword">Kit Image</label>
                         <input type="file" name="image" class="form-control"
                             placeholder="Kit Image">
+                            @error('image')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
                     </div>
        
                         <div class="form-group">
                                 <label for="Status">Kit Price</label>
                                 <input type="number" name="price" class="form-control"
                                     placeholder="Kit Price">
+                                    @error('price')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                         </div>
                         <div class="form-group">
                             <label for="category">Choose category:</label>

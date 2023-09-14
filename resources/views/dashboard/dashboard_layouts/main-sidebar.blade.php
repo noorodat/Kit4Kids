aed33eaa186cddc702a7a7b8e689c844b5743775
Razan Mustafa <razanmustafa8@gmail.com>
2023-09-14 10:47:16 +0300

    razanmustafa

--- resources/views/dashboard/dashboard_layouts/main-sidebar.blade.php
+++ resources/views/dashboard/dashboard_layouts/main-sidebar.blade.php
@@ -16,13 +16,14 @@
           <!-- Sidebar user panel (optional) -->
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-              <img src="{{ asset('dashboard-assets/img/body.png') }}" class="img-circle elevation-2" alt="User Image">
+              {{-- <img src="{{ asset('dashboard-assets/img/body.png') }}" class="img-circle elevation-2" alt="User Image"> --}}
+              <img src="{{ url('/images/' . session('loginimage')) }}" class="img-circle elevation-2" alt="User Image">
 
             </div>
             
             <div class="info">
             <a href="{{ route('admins.index') }}" class="d-block">
-            {{-- {{$admins->name}}  --}}
+            {{session('loginname')}}
             </a>
              </div>
             
